9366968de1196493d31445c29836047d547c3ef6
kebin <kekobin@163.com>
2016-04-19 15:34:18 +0800

    更新。

---
+++ H5-GULP/src/js/main.js
@@ -0,0 +1,7 @@
+var AprilFools = function() {
+  this.baseUrl = "http://www.huya.com/hd/foolsboom/index.php?m=FoolsBoom&do=ajaxGetActivityPanel";
+  this.init();
+};
+
+AprilFools.prototype.init = function() {
+};
